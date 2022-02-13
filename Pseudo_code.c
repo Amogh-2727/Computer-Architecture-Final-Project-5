fd6a90152a34bc535304724994a6d16eed7fe31d
Amogh M Morey <93465807+Amogh-2727@users.noreply.github.com>
2022-02-13 02:47:22 -0800

    Add files via upload
    Pseudo_Code

---
+++ Pseudo_code.c
@@ -0,0 +1,163 @@
+#include<stdio.h>
+
+void int hex-binary(IR){
+	
+}
+
+void int opcode_detection(){
+	hex-binary();
+	opcode;
+	
+}
+
+void int R_type(){
+	
+}
+
+void int I_type(){
+	
+}
+void int S_type(){
+	
+}
+
+void int U_type(){
+	
+}
+
+void int J_type(){
+	
+}
+
+void int read_mem(){
+	IR;
+}
+
+void int write_mem(){
+	
+}
+
+int main(){
+	
+	sp=0;
+	pc=0;
+	ra=0;
+	
+	int x0 - x31;
+	fopen;
+	
+	
+	while (not end of file){
+		
+		read_mem();
+		opcode_detection();
+		
+		switch(opcode){
+			
+			case 1:
+			R_type();
+			
+				decode(IR){
+					if(){
+						write_mem();
+					}
+						
+					else if(){
+						write_mem();
+					}
+			
+			pc+4;
+			
+			break;
+			
+			case 2:
+			I_type();
+			
+				decode(IR){
+					if(){
+						write_mem();
+					}
+						
+					else if(){
+						write_mem();
+					}
+			
+			pc+4;
+			
+			break;
+			
+			case 3:
+			S_type();
+			
+				decode(IR){
+					if(){
+						write_mem();
+					}
+						
+					else if(){
+						write_mem();
+					}
+			
+			pc+4;
+			
+			break;
+			
+			case 4:
+			U_type();
+			
+				decode(IR){
+					if(){
+						write_mem();
+					}
+						
+					else if(){
+						write_mem();
+					}
+			
+			pc+4;
+			
+			break;
+			
+			case 5:
+			J_type();
+			
+				decode(IR){
+					if(){
+						write_mem();
+					}
+						
+					else if(){
+						write_mem();
+					}
+			
+			pc+4;
+			
+			break;
+			
+			case 5:
+			B_type();
+			
+				decode(IR){
+					if(){
+						write_mem();
+					}
+						
+					else if(){
+						write_mem();
+					}
+			
+			pc+4;
+			
+			break;
+		
+			
+		}
+		
+	}
+	
+	return 0;
+}
+		
+		
+		
+	
