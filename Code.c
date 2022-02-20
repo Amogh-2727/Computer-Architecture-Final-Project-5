797337d5dbf201fbfe1488d5aac16667aedbc431
Amogh M Morey <amorey@pdx.edu>
2022-02-20 09:02:15 -0800

    R_type Function

--- Code.c
+++ Code.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 
-void int func_R(int PC){
+int func_R(int PC){
 
 long int instruct=0;
 int rd;
@@ -10,7 +10,7 @@ int rs2;
 int funct7;
 long int temp1,temp2,temp3;
 
-instruct= memory_r[PC] & 0xFF;
+instruct=memory_r[PC]& 0xFF;
 temp1=(instruct<<8);
 temp1=memory_r[PC + 1]& 0xFF;
 temp2=(temp1<<16);
@@ -27,29 +27,81 @@ rs2=(instruct>>5)&& 0x1F;
 funct7=(instruct>>5)&& 0x3F;
 
 switch(funct3){
-    case 0: if(funct7=0) {
+    case 0: {
+        if(funct7=0) {
         Reg[7]=rd;
         Reg[8]=rs1;
-        Reg[]
+        Reg[9]=rs2;
 
-        printf("ADD ");
+        Reg[7]=Reg[8]+Reg[9];
+        printf("ADD R7,R8,R9");
     }
     else{
-        printf("SUB");
+        Reg[7]=rd;
+        Reg[8]=rs1;
+        Reg[9]=rs2;
+
+        Reg[7]=Reg[8]-Reg[9];
+        printf("SUB R7,R8,R9");}
     }
-    case 1: printf("SLL");
-    case 2: printf("SLT");
-    case 3: printf("SLTU");
-    case 4: printf("XOR");
-    case 5: if(funct7=0) {
+    break;
+
+    case 1:{
+         printf("SLL");
+    }
+    break;
+
+    case 2: {
+        printf("SLT");
+    }
+    break;
+
+    case 3: {
+        printf("SLTU");
+    }
+    break;
+
+    case 4: {
+
+        Reg[7]=rd;
+        Reg[8]=rs1;
+        Reg[9]=rs2;
+
+        Reg[7]=((Reg[8]& !Reg[9])|(!Reg[8]& Reg[9]);
+        printf("XOR R7,R8,R9");
+    }
+    break;
+
+    case 5: {
+        if(funct7=0) {
         printf("SRL");
     }
     else{
         printf("SRA");
     }
+    }
+    break;
     
-    case 6: printf("OR");
-    case 7: printf("AND");
+    case 6:{
+
+        Reg[7]=rd;
+        Reg[8]=rs1;
+        Reg[9]=rs2;
+
+        Reg[7]=(Reg[8] | Reg[9]);
+        printf("OR R7,R8,R9");
+    }
+    break;
+
+    case 7: {
+        Reg[7]=rd;
+        Reg[8]=rs1;
+        Reg[9]=rs2;
+
+        Reg[7]=(Reg[8] & Reg[9]);
+        printf("AND R7,R8,R9");
+    }
+    break;
 }
 
 }
