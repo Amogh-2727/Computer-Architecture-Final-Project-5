92e7b89a3fb8a9c025f055374343e807cb8d933e
Ritvik Tiwari <ritviktiwari50@gmail.com>
2022-02-26 17:17:57 -0800

    update

--- defs.h
+++ defs.h
@@ -7,5 +7,11 @@
 uint32_t Reg[32];
 uint32_t PC;
 
+int32_t sign_ext(uint32_t data, uint32_t size)
+{
+    int32_t val = data;
+    val = ((data << (32 - size)) >> (32 - size));
+    return val;
+}
 
 #endif
