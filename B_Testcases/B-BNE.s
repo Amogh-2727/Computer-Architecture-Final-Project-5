47f6314edbbed7c7d6a69762f1d4d8bc98db5545
Arjun Preetham M E Ekalaivan <arjun3@ruby.cecs.pdx.edu>
2022-03-14 00:33:48 -0700

    B_Testcases

---
+++ B_Testcases/B-BNE.s
@@ -0,0 +1,33 @@
+	.file	"B-BNE.c"
+	.option pic
+	.text
+	.align	2
+	.globl	main
+	.type	main, @function
+main:
+	addi	sp,sp,-32
+	sw	s0,28(sp)
+	addi	s0,sp,32
+	li	a5,-20
+	sw	a5,-20(s0)
+	li	a5,20
+	sw	a5,-24(s0)
+	sw	zero,-28(s0)
+	lw	a4,-20(s0)
+	lw	a5,-24(s0)
+	beq	a4,a5,.L2
+	li	a5,1
+	sw	a5,-28(s0)
+	j	.L3
+.L2:
+	li	a5,2
+	sw	a5,-28(s0)
+.L3:
+	li	a5,0
+	mv	a0,a5
+	lw	s0,28(sp)
+	addi	sp,sp,32
+	jr	ra
+	.size	main, .-main
+	.ident	"GCC: (GNU) 9.2.0"
+	.section	.note.GNU-stack,"",@progbits
